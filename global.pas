e73176d1093e4f55fc8f30ee1b7f30ea7fdf3c01
Ana Gauna <amgauna@gmail.com>
2021-10-31 21:01:09 -0300

    Update global.pas

--- global.pas
+++ global.pas
@@ -16,7 +16,7 @@ Sub BtnAnterior.BOF_click()
     DyClientes.MovPrevious
     
     If DyClientes.BOF then
-       Msgbox "Inicio do Arquivo ", 65, "Aviso "
+       Msgbox "Inicio do Arquivo ", 65, " Aviso "
        Dyclientes.MoveNext
     End if
     
@@ -26,5 +26,57 @@ Sub BtnAnterior.BOF_click()
     LeRegistro
 End Sub
 
+Sub BtnApaga_Click()
+    if MsgBox("Conforma a Deleção do Registro", 65, " Urgente ") = 1 then
+       Dyclientes.Delete
+       Dyclientes.movePrevius
+       
+       Rem Se voltou um registro, não é novo
+       NovoRegistro = False
+     
+       LeRegistro
+   End if
+End Sub   
 
+Sub BtnFiltro_Click()
+    Load Frmfiltro
+    Frmfiltro.Show
+    
+    if Dyclientes.BOF then
+       MdgBox "Nenhum registro atendeu a condição", 65, " Aviso "
+       Set DyClientes = BDados.CreateDynaset("Clientes")
+    end if 
+    
+    Dyclientes.MoveFirst
+    LimpaCampos
+    InicializaCampos
+    NovoRegistro = False
+    LeRegistro
+End Sub  
+
+Sub BtnGrava_click()
+    if NovoRegistro then
+    
+       DyClientes.FindFirst "Código = " & txtCódigo.Text
+       
+       if Not DyClientes.NoMath then
+          MagBox "Código Inexistente", 65, " Aviso "
+          exit sub
+       end if
+    end if
+    
+    if NovoRegistro then
+       DyClientes.AddNew
+    else
+       Dyclientes.Edit
+    end if
+    
+    AtribuiCampos
+    DyClientes.Update
+    NovoRegistro = False
+End Sub
+
+
+
+       
 
